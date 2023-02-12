3e81dd5dee6d426123fcb4f915690fd94f56c604
ahsannafis22 <ain7398@gmail.com>
2023-02-12 10:21:43 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Coba Git Remote</title>
+  </head>
+  <body>
+    <h1>Halo San</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem minima ea! Neque natus quis excepturi quo, ratione repudiandae. Odit doloribus nostrum iure laborum est deleniti ipsum reprehenderit tempora fugit.
+    </p>
+  </body>
+</html>
